e288bf212bb3c17d119f55278d29224538e680eb
Edson Luiz Fragnan <edsonl.fragnan@gmail.com>
2019-02-26 00:58:54 -0300

    APP

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,6 +5,39 @@ module.exports = app => {
   const registerRequisition = require('./registerRequisition');
   const message = 'Route not found!';
   let errors = {};
+
+  const validateCPF = (cpf) => {
+    let  numbers, digits, sum, i, result, same_digits;
+    same_digits = 1;
+    if (cpf.length < 11)
+      return false;
+      for (i = 0; i < cpf.length - 1; i++)
+        if (cpf.charAt(i) != cpf.charAt(i + 1)) {
+            same_digits = 0;
+      break;
+        }
+
+    if (!same_digits) {
+      numbers = cpf.substring(0,9);
+      digits = cpf.substring(9);
+      sum = 0;
+      for (i = 10; i > 1; i--)
+        sum += numbers.charAt(10 - i) * i;
+        result = sum % 11 < 2 ? 0 : 11 - sum % 11;
+        if (result != digits.charAt(0))
+          return false;
+        numbers = cpf.substring(0,10);
+        sum = 0;
+        for (i = 11; i > 1; i--)
+          sum += numbers.charAt(11 - i) * i;
+          result = sum % 11 < 2 ? 0 : 11 - sum % 11;
+        if (result != digits.charAt(1))
+          return false;
+      return true;
+    }
+    else
+      return false;
+  };
   
   app.post('/createMass', (req, res) => {
     registerRequisition.requisition();
@@ -18,6 +51,10 @@ module.exports = app => {
     if (!errors.isEmpty()) {
       res.status(422).json({ message: errors.array() });
     } else {
+      let valCPF = validateCPF(req.query.cpf);
+      if (!valCPF) {
+        res.status(422).json({ message: 'Invalid CPF'});
+      }
       registerRequisition.requisition();
       controller.queryCpfClient.consulta(req, res);
     }
@@ -46,6 +83,10 @@ module.exports = app => {
     if (!errors.isEmpty()) {
       res.status(422).json({ message: errors.array() });
     } else {
+      let valCPF = validateCPF(req.params.cpf);
+      if (!valCPF) {
+        res.status(422).json({ message: 'Invalid CPF'});
+      }
       registerRequisition.requisition();
       controller.registerClient.register(req, res);
     }
@@ -59,6 +100,10 @@ module.exports = app => {
     if (!errors.isEmpty()) {
       res.status(422).json({ message: errors.array() });
     } else {
+      let valCPF = validateCPF(req.params.cpf);
+      if (!valCPF) {
+        res.status(422).json({ message: 'Invalid CPF'});
+      }
       registerRequisition.requisition();
       controller.updateClient.update(req, res);
     }
@@ -71,6 +116,10 @@ module.exports = app => {
     if (!errors.isEmpty()) {
       res.status(422).json({ message: errors.array() });
     } else {
+      let valCPF = validateCPF(req.params.cpf);
+      if (!valCPF) {
+        res.status(422).json({ message: 'Invalid CPF'});
+      }
       registerRequisition.requisition();
       controller.deleteClient.delete(req, res);
     }
